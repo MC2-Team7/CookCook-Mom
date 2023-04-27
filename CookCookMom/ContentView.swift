6442d5e0757533055a04acd0400790e2d0134c15
musclefrog <rlatndls113@gmail.com>
2023-04-27 17:16:48 +0900

    hello

--- CookCookMom/ContentView.swift
+++ CookCookMom/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("CookCook, Mom!")
+            Text("RUbiBUriir")
         }
         .padding()
     }

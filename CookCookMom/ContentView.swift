accb2e32850d373191d7534b3cefd9d58eb0a1b9
Kim, Yerim <yerim343@naver.com>
2023-04-27 16:42:41 +0900

    Initial Commit

---
+++ CookCookMom/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CookCookMom
+//
+//  Created by 김예림 on 2023/04/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("CookCook, Mom!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CookCookMom/CookCookMomApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookCookMomApp.swift
+//  CookCookMom
+//
+//  Created by 김예림 on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct CookCookMomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

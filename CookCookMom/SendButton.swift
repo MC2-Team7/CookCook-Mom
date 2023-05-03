a8fbe71e0cab395edf0f3ed7c1faeb4bcbb68863
musclefrog <rlatndls113@gmail.com>
2023-05-03 23:07:47 +0900

    feat: add prototype

---
+++ CookCookMom/SendButton.swift
@@ -0,0 +1,38 @@
+//
+//  SendButton.swift
+//  selectIngredient
+//
+//  Created by 김수인 on 2023/05/01.
+//
+
+import SwiftUI
+
+struct SendButton: View {
+    @State private var showingAlert = false
+    @Binding var selectedIngredients: [Ingredient]
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                showingAlert = true
+                print("재료를 보냈습니다.")
+            }, label: {
+                Text("재료 보내기!")
+                    .frame(width: 380, height: 30)
+                    .font(.title2)
+            })
+            .alert(isPresented: $showingAlert, content: {
+                Alert(title: Text("애기한테 보낼 String: "),
+                      message: Text("\(selectedIngredients[0].description)"))
+            })
+            .buttonStyle(.borderedProminent)
+            .disabled(selectedIngredients.count > 0 ? false : true)
+        }
+    }
+}
+
+//struct SendButton_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SendButton()
+//    }
+//}

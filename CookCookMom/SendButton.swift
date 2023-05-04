feb14c899710d883d6fc1d890c5350bc93bd0506
musclefrog <rlatndls113@gmail.com>
2023-05-04 16:29:43 +0900

    update sendButton

--- CookCookMom/SendButton.swift
+++ CookCookMom/SendButton.swift
@@ -16,17 +16,21 @@ struct SendButton: View {
             Button(action: {
                 showingAlert = true
                 print("재료를 보냈습니다.")
+                
+                for selectedIngredient in selectedIngredients {
+                    print(selectedIngredient.description)
+                }
             }, label: {
                 Text("재료 보내기!")
                     .frame(width: 380, height: 30)
                     .font(.title2)
             })
-            .alert(isPresented: $showingAlert, content: {
-                Alert(title: Text("애기한테 보낼 String: "),
-                      message: Text("\(selectedIngredients[0].description)"))
-            })
-            .buttonStyle(.borderedProminent)
+            //.alert(isPresented: $showingAlert, content: {
+            //    Alert(title: Text("애기한테 보낼 String: "),
+            //          message: Text("\(selectedIngredients[0].description)"))
+            //})
             .disabled(selectedIngredients.count > 0 ? false : true)
+            .buttonStyle(.borderedProminent)
         }
     }
 }

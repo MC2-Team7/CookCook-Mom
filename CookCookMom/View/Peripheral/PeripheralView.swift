3ee2e6c206c120e678275db5c9df06e3dcdb6a4f
Kim, Yerim <yerim343@naver.com>
2023-05-08 14:02:47 +0900

    feat: ble send and fetch data

--- CookCookMom/View/Peripheral/PeripheralView.swift
+++ CookCookMom/View/Peripheral/PeripheralView.swift
@@ -41,8 +41,6 @@ struct PeripheralView: View {
                     }
                 }
             }
-            
-            
         }
         .onDisappear {
             peripheral.stopAction()

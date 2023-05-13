64f578e91832ca4f25fc41410811361316a96dae
musclefrog <rlatndls113@gmail.com>
2023-05-13 21:50:49 +0900

    chore : edit description modal view

--- CookCookMom/View/DescriptionView.swift
+++ CookCookMom/View/DescriptionView.swift
@@ -13,60 +13,92 @@ struct DescriptionView: View {
         NavigationView {
             ScrollView {
                 VStack {
-                    Text("재료 선택하기")
-                        .font(.title3)
-                        .bold()
-                        .foregroundColor(.blue)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding([.leading,.bottom])
-                    Text("오늘 할 요리에 들어갈 재료를 손질할 순서대로 선택해주세요.")
-                        .font(.body)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding([.leading,.bottom])
+                    HStack {
+                        Image("clickCarrot")
+                            .resizable()
+                            .frame(width: 111.31, height: 125.49)
+                            .padding(.leading)
+                        VStack {
+                            Text("재료 선택하기")
+                                .font(.title3)
+                                .bold()
+                                .foregroundColor(.blue)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.leading,.top,.bottom])
+                            Text("오늘 할 요리에 들어갈 재료를 선택해주세요.")
+                                .font(.body)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.leading,.bottom])
+                                .foregroundColor(.secondary)
+                        }
+                        
+                    }
                     
-                    Text("재료 보내기")
-                        .font(.title3)
-                        .bold()
-                        .foregroundColor(.blue)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding([.leading,.bottom,.top])
-                    Text("선택한 재료를 [재료 보내기] 버튼을 눌러 아이에게 보내주세요.")
-                        .font(.body)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding([.leading,.bottom])
+                    HStack {
+                        VStack {
+                            Text("재료 보내기")
+                                .font(.title3)
+                                .bold()
+                                .foregroundColor(.blue)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.leading,.bottom,.top])
+                            Text("선택한 재료를 [재료 보내기] 버튼을 눌러 아이에게 보내주세요.")
+                                .font(.body)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.leading,.bottom])
+                                .foregroundColor(.secondary)
+                        }
+                        Button {
+                        } label: {
+                            Text("재료 보내기")
+                                .frame(width: 150, height: 46.3)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .cornerRadius(30)
+                        }.padding(.trailing)
+                    }
                     
-                    Text("아이와 함께하기")
-                        .font(.title3)
-                        .bold()
-                        .foregroundColor(.blue)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding([.leading,.bottom,.top])
-                    Text("재료를 보내고 아이에게 \"오늘 요리 같이 해볼래?\" 라고 말해주세요. 그리고 아이용 앱에서 흔들리는 알림 아이콘을 누르게 유도해주세요.")
-                        .font(.body)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding([.leading,.bottom])
+                    HStack {
+                        Image("momKid")
+                            .resizable()
+                            .frame(width: 81.39, height: 94)
+                            .padding(.leading)
+                        VStack {
+                            Text("아이와 함께하기")
+                                .font(.title3)
+                                .bold()
+                                .foregroundColor(.blue)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.leading,.bottom,.top])
+                            Text("재료를 보내고 아이에게 \n\"오늘 요리 같이 해볼래?\" \n라고 말해주세요.")
+                                .font(.body)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.leading,.bottom])
+                                .foregroundColor(.secondary)
+                        }
+                    }
                     
-                    Text("아이가 손질한 재료 받기")
-                        .font(.title3)
-                        .bold()
-                        .foregroundColor(.blue)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding([.leading,.bottom,.top])
-                    Text("아이가 재료를 다 손질하면 보내기 버튼을 눌러 부모에게 보낼 수 있도록 알려주세요.")
-                        .font(.body)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading)
+                    HStack {
+                        VStack {
+                            Text("아이 칭찬하기")
+                                .font(.title3)
+                                .bold()
+                                .foregroundColor(.blue)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.leading,.bottom,.top])
+                            Text("아이가 손질한 재료를 받으면 아이를 칭찬해주세요❤️")
+                                .font(.body)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.leading)
+                                .foregroundColor(.secondary)
+                        }
+                        Image("hands")
+                            .resizable()
+                            .frame(width: 120.25, height: 91)
+                            .padding(.trailing)
+                    }
                     
-                    Text("아이 칭찬하기")
-                        .font(.title3)
-                        .bold()
-                        .foregroundColor(.blue)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding([.leading,.bottom,.top])
-                    Text("아이가 손질한 재료를 받으면 아이를 칭찬해주세요❤️ \n\n그 외의 역할놀이에도 사용해도 좋습니다!")
-                        .font(.body)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading)
                     
                 }//VStack
                 .navigationTitle("이렇게 써보세요!")
